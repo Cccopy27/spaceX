39a51fec7a3ab518f1cd64d3c15791ff48cf6700
Cccopy27 <wenhao0627@student.usm.my>
2021-12-18 16:02:59 +0800

    Home and detail componenet added to stack screen for navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StyleSheet, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Detail from './screens/Detail';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -8,7 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Detail" component={Detail}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
