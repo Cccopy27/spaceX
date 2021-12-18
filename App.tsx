f2f0cbe5cec94a60cf62ebc77b0bc362e3b20646
Cccopy27 <wenhao0627@student.usm.my>
2021-12-18 17:36:22 +0800

    graphql config

--- App.tsx
+++ App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackNavigationOptions} from '@react-navigation/native-stack';
+import {StackNavigationOptions} from "@react-navigation/stack";
+import {ApolloClient, InMemoryCache, ApolloProvider,useQuery, gql} from "@apollo/client";
+
 import Home from './screens/Home';
 import Detail from './screens/Detail';
 
 
+//apollo client
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
+
+client.query({
+  query: gql`
+    {
+    rockets {
+      id
+      }
+    }
+  `
+}).then(result => console.log(result));
+
 const Stack = createNativeStackNavigator();
 
-// const screenOptionStyles = () = {
-
-// }
+const screenOptionStyles: NativeStackNavigationOptions ={
+  headerShown: false,
+}
 const App: React.FC=()=> {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+      screenOptions={screenOptionStyles}>
         <Stack.Screen 
         name="Home" 
         component={Home}
-        options={{}}
         />
         <Stack.Screen name="Detail" component={Detail}/>
 
@@ -26,6 +45,8 @@ const App: React.FC=()=> {
     </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
