3a0b766939cb3f08e79939f0a494f83ec3c9849e
Cccopy27 <wenhao0627@student.usm.my>
2021-12-19 21:38:14 +0800

    display all details information in details page

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -8,7 +8,15 @@ const Detail:React.FC= () =>{
     const route = useRoute<detailScreenRouteProp>();
     return (
         <View>
+            <Text>{route.params.name}</Text>
+            <Text>{route.params.company}</Text>
+            <Text>{route.params.country}</Text>
+            <Text>{route.params.description}</Text>
             <Text>{route.params.diameter.meters}</Text>
+            <Text>{route.params.height.meters}</Text>
+            <Text>{route.params.mass.kg}</Text>
+            <Text>{route.params.first_flight}</Text>
+            <Text>{route.params.cost_per_launch}</Text>
         </View>
     )
 }
